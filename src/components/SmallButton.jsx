88ceb5ef063d3cb16d7e8c09bc815c12850e7648
Mohammad Sabur Ali Mahdi <mohammadalimahdi26@gmail.com>
2025-09-28 21:31:18 +0100

    Fixed small text on mobile

--- src/components/SmallButton.jsx
+++ src/components/SmallButton.jsx
@@ -3,11 +3,11 @@
 const SmallButton = () => {
   return (
     <div className="dark:bg-gray-900">
-        <div>
-            <p className="mt-10 mx-auto w-150 text-[1.5rem] text-center">
-                I'm a 2nd Year Computer Science student at Queen Mary University of London, graduating in Summer 2028.
-            </p>
-        </div>
+      <div className="hidden sm:block dark:bg-gray-900">
+        <p className="mt-10 mx-auto w-full max-w-xl text-[1.5rem] text-center">
+          I'm a 2nd Year Computer Science student at Queen Mary University of London, graduating in Summer 2028.
+        </p>
+      </div>
     </div>
   )
 }
